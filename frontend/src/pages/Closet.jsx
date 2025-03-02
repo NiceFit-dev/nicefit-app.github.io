cc1f7f2310d34d07ca7415fcbc87637796403a17
Jose Pablo <rrmax905@gmail.com>
2025-03-01 22:18:11 -0300

    Filtro listo

--- frontend/src/pages/Closet.jsx
+++ frontend/src/pages/Closet.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import CheckboxSelect from "../components/Filter";
 
-function Closet(){
-    return(
-        <div>
+
+const App = () => {
+    const options = [ 
+        {value: "jeans", label: "jeans"},
+        {value: "shirt", label:"shirt"},
+        {value: "jacket", label:"jacket"},
+        {value:"shoes", label:"shoes"}
+    ];
+
+    const handlesSelection = (selectedItems) => {
+        console.log("Selected items:", selectedItems);
+    };
+    return (
+        <div> 
             <h1>Closet</h1>
-            <p>Este es tu closet o armario</p>
+            <p>Este es tu closet o aramario</p>
+            <CheckboxSelect options={options} label="Select cloting" onApply={handlesSelection}/>
         </div>
-    )
-}
-export default Closet;
+    );
+};
+
+export default App;
+
