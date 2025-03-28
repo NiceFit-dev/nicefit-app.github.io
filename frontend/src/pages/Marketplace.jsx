addb1a1c828882616732fcc547ef963f56442d0a
Jose Pablo <rrmax905@gmail.com>
2025-03-27 21:44:25 -0300

    Marketplace arreglado

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import CheckboxSelectMKT from "../components/Filter";
+import CheckboxSelectMKT from "../components/Filter-Mkt";
 
 
 const AppMKT = () => {
@@ -12,9 +12,7 @@ const AppMKT = () => {
     const [SelectedCategoriesMKT, setSeletecCategoriesMKT] = useState([]); // Estado para almacenar selección de filtros
 
     // Filtrar los productos según la categoría seleccionada
-    const filteredProduct = SelectedCategoriesMKT.length > 0
-        ? datageneral.filter(datageneral => SelectedCategoriesMKT.includes(datageneral.category))
-        :datageneral;
+    
 
     // Función que actualiza la selección
     const handlesSelectionMKT = (selectedItems) => {
@@ -25,17 +23,12 @@ const AppMKT = () => {
     return (
     <>
         <div> 
-            <h1>Closet</h1>
-            <p>Este es tu closet o armario</p>
-            <CheckboxSelectMKT options={options} label="Select clothing" onApply={handlesSelectionMKT}/>
+            <h1>Marketplace</h1>
+            <p>Este es tu marketplace</p>
+            <CheckboxSelectMKT options={optionsMKT} label="Select clothing" onApply={handlesSelectionMKT}/>
             
         </div>
-            {filteredProduct.map((datageneral) => (
-                <div
-                    key={datageneral.id} >
-                        <img src={datageneral.url} alt={datageneral.category}/>
-                </div>
-            ))}
+          
         
     
     </>
