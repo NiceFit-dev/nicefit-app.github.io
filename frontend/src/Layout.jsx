e3297537308b88586fd7cfd90e9abad292936eec
Jose Pablo <rrmax905@gmail.com>
2025-03-27 22:09:52 -0300

    Se agregó el jsx al Footer al final

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer.jsx";
 import "./styles/Layout.css"; // Asegúrate de importar el CSS
 
 //Aquí se maneja la estructura de la pagina, lo que mantienen en todo momento el header, navbar, footer, entre otros.
