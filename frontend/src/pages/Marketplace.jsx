02bb1ea520383e85fc9e624f7d88af92662a08be
Jose Pablo <rrmax905@gmail.com>
2025-03-31 22:34:10 -0300

    Marketplace ready

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import CheckboxSelectMKT from "../components/Filter-Mkt";
+import { datageneralMKT } from "../components/Print-Mkt";
 
 
 const AppMKT = () => {
@@ -12,7 +13,10 @@ const AppMKT = () => {
     const [SelectedCategoriesMKT, setSeletecCategoriesMKT] = useState([]); // Estado para almacenar selección de filtros
 
     // Filtrar los productos según la categoría seleccionada
-    
+     // Filtrar los productos según la categoría seleccionada
+     const filteredProductMKT = SelectedCategoriesMKT.length > 0
+     ? datageneralMKT.filter(datageneralMKT => SelectedCategoriesMKT.includes(datageneralMKT.category))
+     :datageneralMKT;
 
     // Función que actualiza la selección
     const handlesSelectionMKT = (selectedItems) => {
@@ -28,6 +32,12 @@ const AppMKT = () => {
             <CheckboxSelectMKT options={optionsMKT} label="Select clothing" onApply={handlesSelectionMKT}/>
             
         </div>
+        {filteredProductMKT.map((datageneralMKT) => (
+                <div
+                    key={datageneralMKT.id} >
+                        <img src={datageneralMKT.url} alt={datageneralMKT.category}/>
+                </div>
+            ))}
           
         
     
