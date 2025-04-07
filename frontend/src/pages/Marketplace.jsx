b690c43e96e1ba2c52c82bd5a59c8f2048166c5d
Diego Leiva <dleiva@ncsu.edu>
2025-04-07 09:22:00 -0400

    Implementación de cartas css en marketplace

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import CheckboxSelect from "../components/Filter"; //El mismo que ocupa closet, componente reutilizable!
 import { datageneralMKT } from "../components/Print-Mkt";
+import "../styles/Marketplace.css"
+
 
 const AppMKT = () => {
   const optionsMKT = [ 
@@ -22,16 +24,23 @@ const AppMKT = () => {
 
   return (
     <>
-      <div>
-        <h1>Marketplace</h1>
-        <p>Este es tu marketplace</p>
-        <CheckboxSelect options={optionsMKT} label="Select clothing" onApply={handleSelection} />
-      </div>
-      {filteredProduct.map((item) => (
-        <div key={item.id}>
-          <img src={item.url} alt={item.category} />
+      <div className="marketplace">
+        <div>
+          <h1>Marketplace</h1>
+          <p>Este es tu marketplace</p>
+          <CheckboxSelect options={optionsMKT} label="Select clothing" onApply={handleSelection} />
         </div>
-      ))}
+        <div className="product-grid">
+          {filteredProduct.map(product => (
+            <div key={product.id} className="product-card">
+              <img src={product.url} alt={product.name} />
+              <p>{product.name}</p>
+            </div>
+            ))}
+        </div>
+
+      </div>
+      
     </>
   );
 };
